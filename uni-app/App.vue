583acb5dc1e9cec54948f77f620ad4ed5f809820
mkinit <im@mkinit.com>
2024-09-12 17:10:59 +0800

    删除分号

--- uni-app/App.vue
+++ uni-app/App.vue
@@ -4,11 +4,11 @@
 			console.log('App Launch')
 			
 			//小程序更新
-			const updateManager = uni.getUpdateManager();
+			const updateManager = uni.getUpdateManager()
 			updateManager.onCheckForUpdate(function(res) {
 				// 请求完新版本信息的回调
-				console.log('请求完新版本信息的回调：',res.hasUpdate);
-			});
+				console.log('请求完新版本信息的回调：',res.hasUpdate)
+			})
 			updateManager.onUpdateReady(function(res) {
 				uni.showModal({
 					title: '更新提示',
@@ -16,15 +16,15 @@
 					success(res) {
 						if (res.confirm) {
 							// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-							updateManager.applyUpdate();
+							updateManager.applyUpdate()
 						}
 					}
-				});
+				})
 
-			});
+			})
 			updateManager.onUpdateFailed(function(res) {
 				// 新的版本下载失败
-			});
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
