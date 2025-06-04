faf214cbd3d0da3fa5df3867b39dce2fe697607d
mkinit <im@mkinit.com>
2025-06-04 22:00:48 +0800

    添加element-plus

--- vue3ts/src/view/home/home.vue
+++ vue3ts/src/view/home/home.vue
@@ -1,18 +1,17 @@
 <template setup>
 	<div class="p-home">
-
 		<form action="">
 			{{count}}
-			<button type="button" @click="count++">count is {{ count }}</button>
+			<el-button type="primary" @click="count++">count is {{ count }}</el-button>
 		</form>
 		首页
 		<div>
-			<button @click="getData">api测试</button>
+			<el-button type="primary" @click="getData">api测试</el-button>
 		</div>
 	</div>
 </template>
 <script setup>
-	import { ref } from 'vue'
+import { ref } from 'vue'
 const count = ref(0)
 import { apiTest } from '@/request/api.ts'
 const getData = () => {
